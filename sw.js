8090247597e96a4c15b6c8b17f9ad1691c64d7fb
vyager-digital <hello@vyager.co>
2026-04-29 22:03:27 +0200

    Auto-commit — 2026-04-29 22:03

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', async () => {
+  const keys = await caches.keys();
+  await Promise.all(keys.map(k => caches.delete(k)));
+  await self.registration.unregister();
+});
